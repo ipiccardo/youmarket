b9c445b58be575f80ea04d2a590dd30ff7f91083
ipiccard <ivan.piccardo@tsoftlatam.com>
2023-11-07 12:05:38 -0300

    Se modifica la importación del header en el layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './styles/globals.scss'
-import Header from '../app/components/header/Header'
+import Header from './components/header/Header'
 import Head from 'next/head'
 
 
