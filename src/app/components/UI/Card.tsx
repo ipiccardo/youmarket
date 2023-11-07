0fc33580b2622ad318867c1f20519583ca51a72e
ipiccard <ivan.piccardo@tsoftlatam.com>
2023-11-07 00:18:39 -0300

    Se agrega un dropdown en la card de la homePage

--- src/app/components/UI/Card.tsx
+++ src/app/components/UI/Card.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import classes from "./card.module.scss";
 import Icon from './icon/Icon';
 import Skeleton from 'react-loading-skeleton'
@@ -11,8 +11,23 @@ import { DataProps } from '@/app/types';
 const Card = () => {
 
     const [data, setData] = useState<DataProps[]>()
-    const [totalBalance, setTotalBalance] = useState(0)
+    const [totalBalance, setTotalBalance] = useState<number>(0)
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+    const [selectedAction, setSelectedAction] = useState('Nombre de Acción')
+    const cardRef = useRef<HTMLDivElement | null>(null);
+
     let totalAmount = 0
+
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
+    const handleActionSelect = (action: string) => {
+        setSelectedAction(action);
+        setIsDropdownOpen(false);
+    };
+
 
     useEffect(() => {
         fetch('/api')
@@ -38,21 +53,44 @@ const Card = () => {
     }, [data])
 
 
+
+    useEffect(() => {
+        function handleDocumentClick(e: any) {
+            if (cardRef.current && !cardRef.current.contains(e.target)) {
+                setIsDropdownOpen(false);
+            }
+        }
+
+        document.addEventListener('click', handleDocumentClick);
+
+        return () => {
+            document.removeEventListener('click', handleDocumentClick);
+        };
+    }, []);
+
     return (
-        <div className={classes.cardContainer}>
+        <div className={classes.cardContainer} ref={cardRef}>
             <div className={classes.amountContainer}>
                 <p>Mi Saldo:  {data ? (<span>${`${totalBalance.toLocaleString('en-US', { style: 'decimal', maximumFractionDigits: 2 })}.00`}</span>) :
                     <Skeleton />}
                 </p>
             </div>
             <div className={classes.campaignContainer}>
-                <p>Mi acción: <span>Nombre de Acción</span></p>
-                <div className={classes.iconContainer}>
+                <p>Mi acción: <span className={classes.dropdownToggle}>{selectedAction}</span></p>
+                <div className={`${classes.iconContainer} ${isDropdownOpen ? classes.rotated : classes.downarrow}`} onClick={toggleDropdown}>
                     <Icon name={'downarrow'} size={20} />
                 </div>
+                {isDropdownOpen && (
+                    <ul className={classes.dropdownMenu}>
+                        <li onClick={() => handleActionSelect('Envío de Premio')}>Envío de Premio</li>
+                        <li onClick={() => handleActionSelect('Carga de Saldo')}>Carga de Saldo</li>
+                    </ul>
+                )}
             </div>
         </div>
     )
 }
 
 export default Card
+
+
