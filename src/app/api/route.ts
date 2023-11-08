3c389ec52304b841b13d04e8773808b44c7138d6
ipiccard <ivan.piccardo@tsoftlatam.com>
2023-11-08 16:42:34 -0300

    Se comenta el push

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -25,28 +25,28 @@ export async function GET() {
   }
 }
 
-export async function PUT(request: any) {
-  const res = await request.json();
+// export async function PUT(request: any) {
+//   const res = await request.json();
 
-  const { newData, newDate } = res;
+//   const { newData, newDate } = res;
 
-  try {
-    const jsonData = fs.readFileSync("./public/data.json", "utf-8");
-    const data = JSON.parse(jsonData);
-    const newObject = {
-      icono: "uparrow",
-      texto: "Carga de Saldo",
-      monto: `+$${newData}`,
-      fecha: newDate,
-    };
-    data.push(newObject);
-    fs.writeFileSync("./public/data.json", JSON.stringify(data, null, 2));
-    return new Response("Valor agregado correctamente", {
-      status: 200,
-    });
-  } catch (error) {
-    return new Response("Error al agregar el valor al archivo JSON", {
-      status: 500,
-    });
-  }
-}
+//   try {
+//     const jsonData = fs.readFileSync("./public/data.json", "utf-8");
+//     const data = JSON.parse(jsonData);
+//     const newObject = {
+//       icono: "uparrow",
+//       texto: "Carga de Saldo",
+//       monto: `+$${newData}`,
+//       fecha: newDate,
+//     };
+//     data.push(newObject);
+//     fs.writeFileSync("./public/data.json", JSON.stringify(data, null, 2));
+//     return new Response("Valor agregado correctamente", {
+//       status: 200,
+//     });
+//   } catch (error) {
+//     return new Response("Error al agregar el valor al archivo JSON", {
+//       status: 500,
+//     });
+//   }
+// }
