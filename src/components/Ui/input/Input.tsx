0528c31dfdeca83f893607a7b4ede11b74ad2592
ipiccard <ivan.piccardo@tsoftlatam.com>
2023-11-09 14:51:03 -0300

    Se valida el regex texto en el input Buscar por nombre o email

--- src/components/Ui/input/Input.tsx
+++ src/components/Ui/input/Input.tsx
@@ -6,9 +6,18 @@ import IconHandler from '../icon/IconHandler'
 import { InputProps } from '@/types'
 
 const Input: React.FC<InputProps> = ({ searchTerm, setSearchTerm }) => {
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newText = e.target.value;
+
+        if (/^[a-zA-Z]*$/.test(newText) || newText === '') {
+            setSearchTerm(newText);
+        }
+    };
+
     return (
         <div className={classes.inputContainer}>
-            <input type='text' value={searchTerm} onChange={(e) => { setSearchTerm(e.target.value) }} className={classes.input} placeholder='Buscar por nombre o email.'></input>
+            <input type='text' value={searchTerm} onChange={handleChange} className={classes.input} placeholder='Buscar por nombre o email.'></input>
             <IconHandler name='search' size={20} />
         </div>
     )
